847a71840c835a8f4b83e0cc192bfb8884f15259
ZakFahey <z.fahey@gmail.com>
2018-12-10 22:51:44 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,9 @@ const monitor = require(`./src/monitors/${process.env.MONITOR}`);
 monitor.check(process.env.IP)
     .then(result => {
         console.log('success');
+        console.log(result);
     })
     .catch(err => {
         console.log('fail');
         console.log(err);
-    });*/
+    });
